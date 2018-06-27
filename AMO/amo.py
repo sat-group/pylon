9a90d11651b26c10ce19118fac3cd321964d85aa
Avital Rabinovitch <arabinov@andrew.cmu.edu>
2018-06-27 10:52:27 -0400

    Bimander Added

--- AMO/amo.py
+++ AMO/amo.py
@@ -36,6 +36,8 @@ class AMO(object):
       return Commander()
     if type == "Product":
       return Product()
+    if type == "Bimander":
+      return Bimander()
     assert 0, "Invalid encoding: " + type
  
   encoding = staticmethod(encoding)
@@ -45,52 +47,50 @@ class AMO(object):
   toString = staticmethod(toString)
  
 class Pairwise(AMO):
-  def build(self, n): 
+  def build(self, formula,variables,lo,hi): 
     formula = []
-    variables = n
 
-    for x in range(1,n+1):
-      for y in range(x+1,n+1):
+    for x in range(lo,hi+1):
+      for y in range(x+1,hi+1):
         formula.append(AMO.binary(-x,-y))
 
-    return AMO.toString(formula, variables)
+    return formula,variables
  
 class Sequential(AMO):
-  def build(self, n): 
+  def build(self, formula,variables,lo,hi): 
     formula = []
-    variables = 2*n-1
+    variables += variables - 1
 
-    for i in range(1,n):
-      formula.append(AMO.binary(-i,n+i))
+    for i in range(lo,hi):
+      formula.append(AMO.binary(-i,hi+i))
       #cnf += "-%d %d 0\n"%(i,n+i)
       if(i > 1):
-        formula.append(AMO.binary(-(n+i-1),n+i))
-        formula.append(AMO.binary(-i,-(n+i-1)))
+        formula.append(AMO.binary(-(hi+i-1),hi+i))
+        formula.append(AMO.binary(-i,-(hi+i-1)))
         # cnf += "-%d %d 0\n"%(n+i-1,n+i)
         # cnf += "-%d -%d 0\n"%(i,n+i-1)
-    formula.append(AMO.binary(-n,-(2*n-1)))
+    formula.append(AMO.binary(-hi,-(2*hi-1)))
     #cnf += "-%d -%d 0\n"%(n,2*n-1)
-    return AMO.toString(formula, variables)
+    return formula, variables
 
 class Binary(AMO):
-  def build(self,n):
+  def build(self, formula,variables,lo,hi):
     formula = []
-    log = int(math.ceil(math.log(n,2)))#auxilary
-    variables = n + log
+    log = int(math.ceil(math.log(hi,2)))#auxilary
+    variables += log
 
     for aux in range(1,log+1):
-      for i in range(1,n+1):
+      for i in range(lo,hi+1):
         if(((i-1) % (2**aux)) < (2**(aux-1))):
-          formula.append(AMO.binary(-i,n+aux))
+          formula.append(AMO.binary(-i,hi+aux))
           # cnf += "-%d %d 0\n"%(i,n+aux)
         else:
-          formula.append(AMO.binary(-i,-(n+aux)))
+          formula.append(AMO.binary(-i,-(hi+aux)))
           # cnf += "-%d -%d 0\n"%(i,n+aux)
-    return AMO.toString(formula, variables)
+    return (formula, variables)
 
 class Commander(AMO):
-  def build(self, n):
-    formula = []
+  def build(self, formula,variables,lo,hi):
     def calcCommander(n):
       if(n <= 3): return 1
       l = math.floor(n / 2)
@@ -99,11 +99,12 @@ class Commander(AMO):
       rightAcc = calcCommander(r)
       return leftAcc+rightAcc
       
-    def genSubOrds(n,cnt):
-      threeCount = n - (cnt * 2)
+    def genSubOrds(lo,hi,cnt):
+      v = hi - lo + 1
+      threeCount = v - (cnt * 2)
       twoCount = cnt - threeCount
       l = []
-      x = 1
+      x = lo
       for i in range(twoCount):
         l.append((x,2))
         x += 2
@@ -112,11 +113,12 @@ class Commander(AMO):
         x += 3
       return l
 
-    comVar = int(calcCommander(n))
-    variables = int(n) + comVar
-    subOrds = genSubOrds(n,comVar)
+    comVar = int(calcCommander(hi-lo+1))
+    comStart = variables + 1
+    variables += comVar
+    subOrds = genSubOrds(lo,hi,comVar)
 
-    c = n+1
+    c = hi+1
     for s in subOrds:
       # at most one variable in group true
       (start,size) = s # inclusive start
@@ -137,19 +139,25 @@ class Commander(AMO):
       for i in range(start,end):
         formula.append(AMO.binary(c,-i))
       c += 1
-
-
+    if (comVar > 2):
+      return self.build(formula,variables,comStart,comStart+comVar);
+    
+    formula.append(AMO.binary(-comStart, -comStart+1));
+    return (formula,variables)
     # exactly on of c true
     # TO-DO implement recursive instead of pairwise
-    for x in range(n+1,n+comVar+1):
-      for y in range(x+1,n+comVar+1):
+    """
+    for x in range(hi+1,hi+comVar+1):
+      for y in range(hi+1,hi+comVar+1):
         formula.append(AMO.binary(-x,-y))
+    
 
 
-    return AMO.toString(formula, variables)
+    return (formula, variables)
+    """  
 
 class Product(AMO):
-  def build(self, n):
+  def build(self, formula,variables,lo,hi):
     p = (math.ceil(math.sqrt(n)))
     q = int(math.ceil(n/p))
     p = int(p)
@@ -173,48 +181,64 @@ class Product(AMO):
            formula.append(AMO.binary(-x,u))
            formula.append(AMO.binary(-x,v))
            x += 1
-    return AMO.toString(formula, variables)
+    return (formula, variables)
 
-class Bimander(AMO)
-  def build(self,n):
+class Bimander(AMO):
+  def build(self, formula,variables,lo,hi):
+
+    def binRep(num,size):
+      b = bin(num)[2:]
+      return (size-len(b))*'0' + b
+
+
+    n = hi - lo +1
     m = int(math.ceil(math.sqrt(n)))
-
-    formula = []
     log = int(math.ceil(math.log(m,2)))#auxilary
     variables = n + log
 
     B = (n // m)
     # A = B + 1
-    sizeA = n % (n // m)
-    sizeB = m - sizeB
+    sizeA = int(math.floor(n/(B+1)))
+    sizeB = m - sizeA
+    
 
+    # make groups
     l = []
-    x = 1
+    x = lo
     for i in range(sizeA):
       l.append((x,B+1))
       x += B+1
     for i in range(sizeB):
       l.append((x,B))
       x += B
-
+    maxLen = len(bin(m)[2:])
+    g = 0
     for s in l:
+      b = binRep(g,maxLen)
       # at most one variable in group true
       (start,size) = s # inclusive start
       end = start+size # exclusive end
-      # TO-DO change to be able to call pairwise
       for x in range(start,end):
-        for y in range(x+1,end):
-          formula.append(AMO.binary(-x,-y))
+        for y in range(maxLen-1,-1,-1):
+          if(b[y] == '0'):
+            formula.append(AMO.binary(-x,-(hi+y+1)))
+          else: # val is 1
+              formula.append(AMO.binary(-x,hi+y+1))
+      g += 1
+
+    return formula,variables
 
 
  
 def build(encoding, n):
   obj = AMO.encoding(encoding)
-  return obj.build(int(n))
+  formula, variables = obj.build([],n,1,int(n))
+  return AMO.toString(formula,variables)
 
 def main():
   obj = AMO.encoding(sys.argv[1])
-  print obj.build(int(sys.argv[2]))
+  formula, variables = obj.build([],int(sys.argv[2]),1,int(sys.argv[2]))
+  print AMO.toString(formula,variables)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
